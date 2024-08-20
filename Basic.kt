ddbbe1c9db162fa2aefb24223dfcac9aa6f59eee
Mamun12209579 <116750608+Mamun000@users.noreply.github.com>
2024-08-20 21:20:03 +0530

    Add files via upload

---
+++ Basic.kt
@@ -0,0 +1,41 @@
+fun main(){
+       println("Hello World!")
+
+       //This is Single Line comment
+
+       /*This is
+       multiline 
+       comment */
+
+       var a=20 //It can be reintialized
+       val b=10//Its a constant value throughout the programe
+       println(a)
+       println(b)
+       println(a+b)//Basic operation
+       a=10
+       print(a)
+
+
+       //Types of Data
+
+       var j=10//Int
+       //if want to add the data type
+       var J:Int=10//Int
+       var k=10.00//Float
+       var l="LPU"//String
+       var m='A'//char
+
+       println(j)
+       println(k)
+       println(l)
+       println(m)
+
+       
+       var string1="Hello\nI'm Learning\t Kotlin";
+       var string2="""Hello
+       I'm Learning Kotlin"""
+       println(string1)
+       println(string2)
+
+       
+}
