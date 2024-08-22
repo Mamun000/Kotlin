131c17e9a513e2455145e6973d0c224708ca0ddc
Mamun12209579 <116750608+Mamun000@users.noreply.github.com>
2024-08-22 13:07:51 +0530

    Add files via upload

---
+++ TypeConverstion.kt
@@ -0,0 +1,12 @@
+fun main() {
+    val a: String = "hello"
+    val b: String = a as String // Safe casting
+    println(b)
+
+    // Unsafe cast (commented out because it will cause a crash)
+    // val c: Int = a as Int // Will throw ClassCastException
+
+    // Safe casting with nullable type
+    val d: Int? = a as? Int // Will result in 'null'
+    println(d) // Prints 'null'
+}
