8260fe27caa6cf335d8620ead1668abc94f66cc0
Mamun <116750608+Mamun000@users.noreply.github.com>
2024-09-04 10:15:35 +0530

    Add files via upload

---
+++ Interface.kt
@@ -0,0 +1,24 @@
+interface Shape{
+    fun area():Double
+}
+open class Rectangles(val a:Double,val b:Double):Shape{
+    override fun area():Double{
+        return a*b
+    }
+    open fun display(){
+        println("Area of rectangle with dimension $a and $b is ${area()}")
+    }
+}
+
+class Squares(side:Double):Rectangle(side,side){
+    override fun display() {
+        println("Area of square with side $a is ${area()}")
+    }
+}
+
+fun main(){
+    val myRectangle=Rectangle(4.0,5.0)
+    myRectangle.display()
+    val mySquare=Squares(3.0)
+    mySquare.display()
+}
